5a2807906933348a602d130f443586fbba97db69
Paulo Clavijo Esteban <paucls@gmail.com>
2017-02-22 21:03:41 +0000

    Initialized SpringBoot 1.4 project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Order Management
+An Order Management API microservice use to put in practice DDD concepts. Uses Spring Boot, Swagger, Spring Data JPA, H2 Database.
+
+## Overview  
+Start your server as an simple java application  
+
+You can view the api documentation in swagger-ui by pointing to  
+http://localhost:8080/  
+
+## Demo
+
+## Documentation
+Links to some of the articles and documentation used to implement this project:
+
+1.0.0
+- 
---
+++ src/main/java/io/ordermanagement/configuration/SwaggerDocumentationConfig.java
@@ -0,0 +1,35 @@
+package io.ordermanagement.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerDocumentationConfig {
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Order Management API")
+                .description("A Order Management API microservice implemented in Spring Boot")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.ordermanagement.api"))
+                .build()
+                .directModelSubstitute(org.joda.time.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(org.joda.time.DateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo());
+    }
+
+}
