5935c0c161eade9c1787f35c7100b3dc77eb9edb
Paulo Clavijo Esteban <paucls@gmail.com>
2017-09-12 22:14:41 +0100

    All commands should implement Command interface

--- src/main/java/io/cqrs/taskmanagement/domain/model/runbook/CompleteRunbook.java
+++ src/main/java/io/cqrs/taskmanagement/domain/model/runbook/CompleteRunbook.java
@@ -1,9 +1,10 @@
 package io.cqrs.taskmanagement.domain.model.runbook;
 
+import io.cqrs.taskmanagement.domain.model.Command;
 import lombok.Value;
 
 @Value
-public class CompleteRunbook {
+public class CompleteRunbook implements Command {
     String runbookId;
     String userId;
 }
--- src/main/java/io/cqrs/taskmanagement/domain/model/runbook/CreateRunbook.java
+++ src/main/java/io/cqrs/taskmanagement/domain/model/runbook/CreateRunbook.java
@@ -1,9 +1,10 @@
 package io.cqrs.taskmanagement.domain.model.runbook;
 
+import io.cqrs.taskmanagement.domain.model.Command;
 import lombok.Value;
 
 @Value
-public class CreateRunbook {
+public class CreateRunbook implements Command {
     String projectId;
     String runbookId;
     String name;
