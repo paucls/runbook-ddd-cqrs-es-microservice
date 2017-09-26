cfd5221a233bb94f764c88f83fdec5007fd1d662
Paulo Clavijo Esteban <paucls@gmail.com>
2017-09-26 20:31:00 +0100

    Refactor extracting projection logic to a separate class

--- src/main/java/io/cqrs/taskmanagement/read/model/runbook/JpaTaskQueries.java
+++ src/main/java/io/cqrs/taskmanagement/read/model/runbook/JpaTaskQueries.java
@@ -1,31 +1,23 @@
 package io.cqrs.taskmanagement.read.model.runbook;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.cqrs.taskmanagement.domain.model.runbook.TaskAdded;
-import io.cqrs.taskmanagement.port.adapter.persistence.JpaEventStore;
 import io.cqrs.taskmanagement.port.adapter.restapi.TaskDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 import java.util.stream.Collectors;
 
 @Service
-public class JpaTaskQueries implements TaskQueries, Observer {
+public class JpaTaskQueries implements TaskQueries {
 
     private TaskRepository taskRepository;
-
     private ObjectMapper objectMapper;
 
     @Autowired
-    public JpaTaskQueries(JpaEventStore jpaEventStore, TaskRepository taskRepository) {
+    public JpaTaskQueries(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
         this.objectMapper = new ObjectMapper();
-
-        // Subscribe Read Model to new Events
-        jpaEventStore.addObserver(this);
     }
 
     public List<TaskDto> tasksForRunbook(String runbookId) {
@@ -34,26 +26,5 @@ public class JpaTaskQueries implements TaskQueries, Observer {
         return tasks.stream()
                 .map(entity -> objectMapper.convertValue(entity, TaskDto.class))
                 .collect(Collectors.toList());
-    }
-
-    @Override
-    public void update(Observable observable, Object object) {
-        // Handles only Events this Read Model cares about
-        if (object instanceof TaskAdded) {
-            handleTaskAdded((TaskAdded) object);
-        }
-    }
-
-    private void handleTaskAdded(TaskAdded event) {
-        TaskEntity task = new TaskEntity(
-                event.getRunbookId(),
-                event.getTaskId(),
-                event.getAssigneeId(),
-                event.getName(),
-                event.getDescription(),
-                null
-        );
-
-        taskRepository.save(task);
     }
 }
---
+++ src/main/java/io/cqrs/taskmanagement/read/model/runbook/RunbookTasksProjection.java
@@ -0,0 +1,48 @@
+package io.cqrs.taskmanagement.read.model.runbook;
+
+import io.cqrs.taskmanagement.domain.model.runbook.TaskAdded;
+import io.cqrs.taskmanagement.port.adapter.persistence.JpaEventStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Read Model Projection
+ */
+@Component
+public class RunbookTasksProjection implements Observer {
+
+    private TaskRepository taskRepository;
+
+    @Autowired
+    public RunbookTasksProjection(JpaEventStore jpaEventStore, TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+
+        // Subscribe Read Model to new Events
+        jpaEventStore.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable observable, Object object) {
+        // Handles only Events this Projection cares about
+        if (object instanceof TaskAdded) {
+            handleTaskAdded((TaskAdded) object);
+        }
+    }
+
+    private void handleTaskAdded(TaskAdded event) {
+        TaskEntity task = new TaskEntity(
+                event.getRunbookId(),
+                event.getTaskId(),
+                event.getAssigneeId(),
+                event.getName(),
+                event.getDescription(),
+                null
+        );
+
+        taskRepository.save(task);
+    }
+
+}
